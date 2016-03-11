39b7413e05a1222a05b2dcfefd9d61bda07167b0
Itay Karo <karoitay@gmail.com>
2016-03-11 21:13:23 +0200

    project 4

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -9,3 +9,72 @@
 // program clears the screen, i.e. writes "white" in every pixel.
 
 // Put your code here.
+
+(CACHE)
+  @prev_val
+  M=0
+
+(LOOP)
+  @KBD
+  D=M;
+  @UNPRESSED
+  D;JEQ
+
+(PRESSED)
+  @val
+  M=-1
+  @SHOULD_PAINT
+  0;JMP
+
+(UNPRESSED)
+  @val
+  M=0
+
+(SHOULD_PAINT) 
+  @val
+  D=M
+  @prev_val
+  D=D+M
+  @LOOP
+  D;JEQ
+
+(SET_PREV_VAL)
+  @val
+  D=M
+  @prev_val
+  M=D
+
+(PAINT)
+  @i
+  M=0
+
+  (PAINT_LOOP)
+    @i
+    D=M
+    @8192 // screen size
+    D=D-A
+    @END
+    D;JGE
+
+    @SCREEN
+    D=A
+    @i
+    D=D+M
+    @screen_add
+    M=D
+
+    @val
+    D=M
+    @screen_add
+    A=M
+    M=D
+
+    @i
+    M=M+1
+
+    @PAINT_LOOP
+    0;JMP
+
+(END)
+  @LOOP
+  0;JMP
--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -7,3 +7,60 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+(OPTIMIZE) // copy RAM[0], RAM[1] to m, n such that m <= n
+  @R0
+  D=M
+  @R1
+  D=D-M
+  @R0GR1
+  D;JGE
+
+  (R1GR0)
+    @R0
+    D=M
+    @m
+    M=D
+    @R1
+    D=M
+    @n
+    M=D
+    @COMPUTE
+    0;JMP
+  (R0GR1)
+    @R0
+    D=M
+    @n
+    M=D
+    @R1
+    D=M
+    @m
+    M=D
+
+(COMPUTE) // now RAM[0] >= RAM[1]
+  @R2
+  M=0
+  @i
+  M=0
+
+  (LOOP)
+    @i
+    D=M
+    @m
+    D=M-D
+    @END
+    D;JEQ
+
+    @n
+    D=M
+    @R2
+    M=M+D
+
+    @i
+    M=M+1
+    @LOOP
+    0;JMP
+
+(END)
+  @END
+  0;JMP
